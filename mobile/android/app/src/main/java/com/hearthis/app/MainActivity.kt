f35a998b22ebcd3c87d909cb589cab39ead8e3fb
chirag jeevanani <chiragjeevanani333@gmail.com>
2026-08-27 21:07:12 +0530

    Fix AppRegistry root component name to 'main' in MainActivity.kt and index.js

--- mobile/android/app/src/main/java/com/hearthis/app/MainActivity.kt
+++ mobile/android/app/src/main/java/com/hearthis/app/MainActivity.kt
@@ -7,7 +7,7 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-    override fun getMainComponentName(): String = "Hear This"
+    override fun getMainComponentName(): String = "main"
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
--- mobile/index.js
+++ mobile/index.js
@@ -1,4 +1,8 @@
-import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
 import App from './App';
 
-registerRootComponent(App);
+// Register all possible root component names to prevent launch crashes
+AppRegistry.registerComponent('main', () => App);
+AppRegistry.registerComponent('Hear This', () => App);
+AppRegistry.registerComponent('HearThis', () => App);
+AppRegistry.registerComponent('hear-this', () => App);
